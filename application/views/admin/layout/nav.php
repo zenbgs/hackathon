28c5a0d9cc809ca35fb976ef5470e2cf9190c953
root <root@ip-172-31-22-83.ap-southeast-1.compute.internal>
2023-09-24 00:38:43 +0000

    latest change

--- application/views/admin/layout/nav.php
+++ application/views/admin/layout/nav.php
@@ -22,9 +22,9 @@
         <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
           <a href="javascript:;" class="nav-link text-body p-0" id="iconNavbarSidenav">
             <div class="sidenav-toggler-inner">
-              <i class="sidenav-toggler-line"></i>
-              <i class="sidenav-toggler-line"></i>
-              <i class="sidenav-toggler-line"></i>
+              <i class="sidenav-toggler-line" style="background: white;"></i>
+              <i class="sidenav-toggler-line" style="background: white;"></i>
+              <i class="sidenav-toggler-line" style="background: white;"></i>
             </div>
           </a>
         </li>
